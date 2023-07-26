c3c150c6c2a6f2d75d3343094d392bcc7fd147a9
benjamin <benjamin.edgard@gmail.com>
2023-07-26 13:41:07 +0200

    partie 5

--- backend/controllers/likes.js
+++ backend/controllers/likes.js
@@ -1,53 +1,135 @@
 
-//importer le modèle sauce
+
+
+
+// importer le modèle sauce
 const Sauce = require('../models/Products');
 
 
 //permet de liker ou disliker une sauce
 exports.likeDislike = (req, res, next) => {
-    const test = req.body
-    console.log("test:", test);
 
-    const like = req.body.like
+    //les variables correspondantes aux données du body et de params
+
+    const like = req.body.like //récupération du like ou du dislike
     console.log("like:", like);
-
-    const userId = req.body.userId;
+    const userId = req.body.userId; //récupération de l'userId
     console.log("userId :", userId);
+    const sauceId = req.params.id; //récupération de l'id
+    console.log("sauceId:", sauceId);
 
-    const sauceId = req.params.id;
-    console.log("sauceId:", id);
+    // pour ajouter un like
 
+    if (like === 1) { //si le like est égale à 1
+        Sauce.updateOne({ // sélection de la sauce à modifier
+            _id: sauceId
+        }),
+            {
+                $push: {
+                    usersLiked: userId //ajout de l'userId
+                },
+
+                $inc: {
+                    likes: +1 //incrémentation de 1
+                }
+            }
+                .then(() => res.status(200).json({ message: "j'ajoute un like!" }))
+                .catch(error => res.status(400).json({ error }))
+    }
+
+
+    //pour ajouter un dislike
+    if (like === -1) { //si le like égale à -1
+        Sauce.updateOne({ // sélection de la sauce à modifier
+            _id: sauceId
+        }),
+            {
+                $push: {
+                    dislikes: userId //ajout de l'user Id
+                },
+
+                $inc: {
+                    dislikes: -1 // incrémentation de -1
+                }
+
+            }
+                .then(() => res.status(200).json({ message: "j'ajoute un dislike!" }))
+                .catch(error => res.status(400).json({ error }))
+    }
+
+    //pour décrémenter un like ou un dislike et supprimer l'userId
+    if (like === 0) {
+        Sauce.findOne({ _id: sauceId })
+            .then(sauce => {
+                if (sauce.usersLiked.includes(userId)) { //si la base de données contient déjà un userId
+                    Sauce.updateOne({
+                        _id: sauceId
+                    }),
+
+                        {
+                            $pull: {
+                                usersLiked: userId //supprimer l'userId
+                            },
+
+                            $inc: {
+                                likes: -1 //décrémenter de 1
+                            }
+
+                        }
+                            .then(() => res.status(200).json({ message: "je supprime un like!" }))
+                            .catch(error => res.status(400).json({ error }))
+
+                }
+
+                if (sauce.usersDisliked.includes(userId)) { //si la base de données contient déjà un userId
+                    Sauce.updateOne({
+                        _id: sauceId
+                    }),
+
+                        {
+                            $pull: {
+                                usersDisliked: userId //supprimer l'userId
+                            },
+
+                            $inc: {
+                                dislikes: -1 //décrémenter de 1
+                            }
+
+                        }
+                            .then(() => res.status(200).json({ message: "je supprime un dislike!" }))
+                            .catch(error => res.status(400).json({ error }))
+                }
+            })
+
+            .catch(error => res.status(400).json({ error }))
+
+    }
 }
 
 
 
 
-// Increment:
-// Inventory.findOneAndUpdate({ _id: mongoose.Types.ObjectId(id) }, { $inc: { quantity: 1 } }, { new: true }).then((results) => { console.log(results) });
-
-// Decrement:
-// Inventory.findOneAndUpdate({ _id: mongoose.Types.ObjectId(id) }, { $inc: { quantity: -1 } }, { new: true }).then((results) => { console.log(results) })
 
 
 
-// Définit le statut « Like » pour
-// l' userId fourni. Si like = 1,
-// l'utilisateur aime (= like) la
-// sauce. Si like = 0, l'utilisateur
-// annule son like ou son
-// dislike. Si like = -1,
-// l'utilisateur n'aime pas (=
-// dislike) la sauce. L'ID de
-// l'utilisateur doit être ajouté
-// ou retiré du tableau
-// approprié. Cela permet de
-// garder une trace de leurs
-// préférences et les empêche
-// de liker ou de ne pas disliker
-// la même sauce plusieurs
-// fois : un utilisateur ne peut
-// avoir qu'une seule valeur
-// pour chaque sauce. Le
-// nombre total de « Like » et
-// de « Dislike » est mis à jour à
-// chaque nouvelle notation.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -8,13 +8,13 @@ dotenv.config();
 module.exports = (req, res, next) => {
     try {
 
-        const token = req.headers.authorization.split(' ')[1]; //localisation du token
+        const token = req.headers.authorization.split(' ')[1]; //récupération du token en le
         const decodedToken = jwt.verify(token, `${process.env.TOKEN}`);
         const userId = decodedToken.userId;
 
         //comparer l'userId de la requête à celui du token
         if (req.body.userId && req.body.userId !== userId) {
-            throw "User ID n'est pas valide"
+            throw "User ID n'est pas valide"  // si le token ne correspond à l'userId
 
         } else {
             req.auth = {
@@ -23,7 +23,7 @@ module.exports = (req, res, next) => {
         };
         next()
 
-    } catch (error) {
+    } catch (error) { //problème d'authentification 
         res.status(401).json({ error })
     }
 
